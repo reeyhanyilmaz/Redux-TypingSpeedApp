5c4cb3748d611375b8c93385e29ea7f46ad0b415
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2022-07-17 11:59:48 +0300

    add Input component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Clock from './components/Clock';
 import TextArea from './components/TextArea';
+import Input from './components/Input';
 
 function App() {
   return (
     <div className="App">
       <Clock />
       <TextArea />
+      <Input />
     </div>
   );
 }
---
+++ src/components/Input/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Input() {
+  return (
+    <div className='flex items-center justify-center mt-12'>
+        <input type="text" placeholder="typing..." className='border-indigo-300 border-4 border-solid rounded-md w-1/3 h-20 p-3'/>
+    </div>
+  )
+}
+
+export default Input;
