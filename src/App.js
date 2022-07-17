d9ef3e30a888798e3c422ba2c3efb647769a6b4f
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2022-07-17 11:52:58 +0300

    add TextArea

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Clock from './components/Clock';
+import TextArea from './components/TextArea';
 
 function App() {
   return (
     <div className="App">
       <Clock />
+      <TextArea />
     </div>
   );
 }
