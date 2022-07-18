4fedcfb10b797df7c1d7c0304c7a66ad08f9cf75
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2022-07-18 18:16:43 +0300

    add words map func.

--- src/components/TextArea/index.js
+++ src/components/TextArea/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 function TextArea() {
+  const words = useSelector(state => state.typingSpeed.words);
+  const selectedLang = useSelector(state => state.typingSpeed.selectedLang);
+ console.log('selectedLang ', selectedLang);
+
   return (
     <div className="flex items-center justify-center mt-40">
         <div className='border-indigo-300 border-4 border-double rounded-xl w-1/2 h-40 p-3'>
+          {
+           words.map((letter, index) => {
+              return (
+                <span key={index} className='text-indigo-300'>  
+                  {letter[selectedLang]}{" "}
+                </span>
+              )
+            })
+          }
         </div>
     </div>
   )
