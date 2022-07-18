8a04253c169a423204fd3d8ca4409e66768ad708
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2022-07-18 18:16:57 +0300

    add setSelectedLang

--- src/redux/typingSpeedSlice.js
+++ src/redux/typingSpeedSlice.js
@@ -1,18 +1,25 @@
 import { createSlice } from "@reduxjs/toolkit";
+import words from "./words.json"
 
 export const typingSpeedSlice = createSlice({
     name: "typingSpeed",
     initialState: {
-        typingSpeed: "",
+        words: words.words,
+        lang: [ "targetWord" ,  "englishWord"],    
+        selectedLang: "targetWord",
         key: 0,
     },
     reducers: {
-        setKey: (state, action) => {
+        setKey: (state) => {
             state.key += 1
-
-        }
+        },
+        setSelectedLang: (state, action) => {
+            const langFind = state.lang.find(item => item === action.payload);
+            console.log('langFind', langFind);
+            state.selectedLang = langFind;
+        },
     },
 })
 
-export const { setKey } = typingSpeedSlice.actions;
+export const { setKey, setSelectedLang } = typingSpeedSlice.actions;
 export default typingSpeedSlice.reducer;
