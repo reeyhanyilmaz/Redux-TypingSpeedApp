6a07dfca2934cd36b318432a3c73ab09a9a0166f
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2022-08-27 15:47:20 +0300

    add swal game over alert

--- src/components/Clock/index.js
+++ src/components/Clock/index.js
@@ -1,8 +1,9 @@
 import React, { useEffect } from "react";
 import { CountdownCircleTimer } from "react-countdown-circle-timer";
 import styled from "styled-components";
-import { useSelector} from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import Swal from "sweetalert2/dist/sweetalert2.js";
+import { setReplay } from "../../redux/typingSpeedSlice";
 
 //sayfayı ortalıyor
 const TimerWrapper = styled.div`
@@ -15,35 +16,34 @@ const TimerWrapper = styled.div`
 function Clock() {
   const time = useSelector((state) => state.typingSpeed.time);
   const start = useSelector((state) => state.typingSpeed.start);
+  const wrongWord = useSelector((state) => state.typingSpeed.wrongWord);
+  const correctWord = useSelector((state) => state.typingSpeed.correctWord);
 
   //swal
   const Swal = require("sweetalert2");
 
+  //dispatch
+  const dispatch = useDispatch();
+
   const renderTime = ({ remainingTime }) => {
     if (remainingTime === 0) {
-      return <div>Too late...</div>;
+      Swal.fire({
+        text: "Game Over!",
+        title: `✅ ${correctWord} 🚫 ${wrongWord}`,
+      }).then((confirmButton) => {
+        if (confirmButton.value) {
+          dispatch(setReplay());
+        }
+      });
     } else {
       return (
-        <div>{remainingTime} <br />
+        <div>
+          {remainingTime} <br />
           <span className="text-slate-400"> seconds </span>
         </div>
       );
     }
   };
-
-  // useEffect(() => {
-  //   if (time === 0) {
-  //     Swal.fire({
-  //       title: "Final",
-  //       showClass: {
-  //         popup: "animate__animated animate__fadeInDown",
-  //       },
-  //       hideClass: {
-  //         popup: "animate__animated animate__fadeOutUp",
-  //       },
-  //     });
-  //   }
-  // }, [time]);
 
   return (
     <div className="flex items-center justify-center">
